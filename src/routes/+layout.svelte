ac8d55402a43841dda572260f85d1ebb8613ea6b
Lynn <lynn89sudo@gmail.com>
2025-12-25 05:27:07 +0000

    Commit 1

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+
+	import "$lib/styles.css";
+
+	let title = $state("Orpheus <3")
+</script>
+
+<svelte:head> 
+	<title>{title}</title>
+	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Geom:ital,wght@0,300..900;1,300..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playwrite+FR+Trad:wght@100..400&display=swap" rel="stylesheet">
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+    import { onMount } from "svelte";
+    import { cubicInOut } from "svelte/easing";
+    import { fly } from "svelte/transition";
+
+    let toggle = $state(false);
+
+    onMount(() => {
+        setTimeout(() => {toggle = true}, 100);
+    })
+</script>
+<style>
+    #card {
+        position: fixed;
+        background-color: aliceblue;
+        top: 50px;
+        bottom: 50px;
+        right: 70px;
+        left: 70px;
+        transform: rotate(0.5deg);
+        border-radius: 20px;
+        box-shadow: 5px 10px rgb(197, 181, 181);
+
+        animation: pulse 3s infinite ease-in-out;
+        transition: box-shadow 0.3s ease-in-out;
+    }
+    #card:hover {
+        box-shadow: 10px 15px rgb(167, 152, 152);
+    }
+    @keyframes pulse {
+        0% {
+            transform: rotate(0.5deg);
+            
+        }
+        50% {
+            transform: rotate(1deg);
+        }
+        100% {
+            transform: rotate(0.5deg);
+        }
+    }
+
+    #content {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+</style>
+{#if toggle}
+    <div id="card" transition:fly={{y:-200, easing: cubicInOut}}>
+        <div id="content">
+            <h1>Content</h1>
+        </div>
+    </div>
+{/if}
