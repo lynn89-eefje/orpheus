efa281b3537ae939f5310f616b5fca58aed13854
Lynn <lynn89sudo@gmail.com>
2025-12-26 06:06:32 +0000

    Deploy 2 - Fix Sizing Issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
     })
 
     let content = $state("");
-    let page = $state(0);
+    let page = $state(3);
     let pagesEnd = 4;
 </script>
 <style>
@@ -59,12 +59,13 @@
         margin-left: 30px;
         margin-right: 30px;
     }
-    @media screen and (max-width: 900px) {
+
+    @media screen and (max-width: 800px) {
         #page.page2 h1 {
-            font-size: 30px !important;
+            font-size: 15px;
         }
         #page.page2 h3 {
-            font-size: 10px;
+            font-size: 14px;
         }
     }
 
@@ -96,11 +97,11 @@
             <div id="page" class="page2">
                     <div style="transform: translateY(-50px)">
                         <h3 in:slide={{delay:1000}} out:slide>you didn't just plunder doubloons,</h3>
-                        <h3 in:slide={{delay:2000}} out:slide>you also stole</h3>
-                        <h1 in:slide={{delay:2700}} out:slide style="font-size: 50px; font-weight: 900; margin: 0;">MY HEART</h1>
+                        <h3 in:slide={{delay:3000}} out:slide>you also stole</h3>
+                        <h1 in:slide={{delay:3700}} out:slide style="font-size: 70px; font-weight: 900; margin: 0;">MY HEART</h1>
                     </div>
             </div>
-            <img in:fly={{delay:3500, y:50}} out:fly={{y:50}} src="images/chest.png" alt="Treasure Chest" style="position: absolute; transform: translate(-50%, -50%); left: 50%; bottom: 30px; max-width: 250px;"/>
+            <img in:fly={{delay:4500, y:100}} out:fly={{y:100}} src="images/chest.png" alt="Treasure Chest" style="position: absolute; transform: translate(-50%, -50%); left: 50%; bottom: 40px; max-width: 300px;"/>
             {/if}
             <div id="buttons">
                 {#if page > 0}<button onclick={() => {page--}}><span translate = "no" class="material-symbols-outlined">arrow_circle_left</span></button>{/if}{#if page < pagesEnd}<button onclick={() => {page++}}><span translate = "no" class="material-symbols-outlined">arrow_circle_right</span></button>{/if}
@@ -111,22 +112,22 @@
             <svg width="110" height="110" style="position: fixed; bottom: 200px; right: 5px;">
                 <path d="M 62.4689 11.0223 C 59.0884 11.8651 57.0936 15.2564 57.9238 18.5865 c 1.6983 6.8115 9.0005 6.8665 15.3155 12.4198 c 2.7795 -7.7671 9.378 -11.6096 7.7426 -18.1688 c -0.8303 -3.3301 -4.2342 -5.3753 -7.5642 -4.5451 c -2.4219 0.6038 -4.1887 2.5449 -4.6316 4.8526 c -1.4744 -1.8297 -3.8953 -2.7266 -6.3172 -2.1228 z" fill="black"/>
             </svg>
-            <h2 style="user-select: none; -webkit-user-select: none; position:fixed; right: 40px; bottom: 30px; transform: rotate(-5deg)">^w^</h2>
-            <h2 style="user-select: none; -webkit-user-select: none; position:fixed; right: 40px; top: 30px; transform: rotate(8deg)">:3</h2>
-            <h2 style="user-select: none; -webkit-user-select: none; position:fixed; left: 40px; top: 90px; transform: rotate(-5deg)">{">~<"}</h2>
-            <h2 style="user-select: none; -webkit-user-select: none; position:fixed; right: 30px; bottom:180px; transform: rotate(-5deg)"><span class="material-symbols-outlined">kid_star</span></h2>
+            <h2 class="decor" style="user-select: none; -webkit-user-select: none; position:fixed; right: 40px; bottom: 30px; transform: rotate(-5deg)">^w^</h2>
+            <h2 class="decor" style="user-select: none; -webkit-user-select: none; position:fixed; right: 40px; top: 30px; transform: rotate(8deg)">:3</h2>
+            <h2 class="decor" style="user-select: none; -webkit-user-select: none; position:fixed; left: 40px; top: 90px; transform: rotate(-5deg)">{">~<"}</h2>
+            <h2 class="decor" style="user-select: none; -webkit-user-select: none; position:fixed; right: 30px; bottom:180px; transform: rotate(-5deg)"><span class="material-symbols-outlined" style:font-size="50px;">kid_star</span></h2>
 
-            <h2 style="user-select: none; -webkit-user-select: none; position:fixed; left: 40px; bottom:180px; transform: rotate(-5deg)"><span class="material-symbols-outlined">kid_star</span></h2>
-            <h2 style="user-select: none; -webkit-user-select: none; position:fixed; right: 20px; top:170px; transform: rotate(9deg);"><span class="material-symbols-outlined" style:font-size="60px;">kid_star</span></h2>
+            <h2 class="decor" style="user-select: none; -webkit-user-select: none; position:fixed; left: 40px; bottom:180px; transform: rotate(-5deg)"><span class="material-symbols-outlined" style:font-size="60px;">kid_star</span></h2>
+            <h2 class="decor" style="user-select: none; -webkit-user-select: none; position:fixed; right: 20px; top:170px; transform: rotate(9deg);"><span class="material-symbols-outlined" style:font-size="60px;">kid_star</span></h2>
 
             <svg width="80" height="80" style="position: fixed; bottom:90px; right: 10px;">
                 <path d="M 15.6931 3.0273 C 13.1911 2.0669 10.4378 3.3237 9.4918 5.7884 c -1.9352 5.0413 2.6418 8.2979 4.1708 14.5833 c 5.1784 -3.6678 11.0315 -3.1777 12.895 -8.0323 c 0.9461 -2.4647 -0.2964 -5.2553 -2.761 -6.2014 c -1.7925 -0.6881 -3.7623 -0.2445 -5.0595 1.0142 c -0.1218 -1.8034 -1.2515 -3.4368 -3.044 -4.1248 z" stroke="black" fill="black"/>
             </svg>
-            <h2 style="user-select: none; -webkit-user-select: none; position:fixed; left: 40px; bottom: 40px; transform: rotate(8deg)">:P</h2>
-            <h2 style="user-select: none; -webkit-user-select: none; position:fixed; left: 30px; top: 130px; transform: rotate(8deg)">hehe</h2>
-            <h2 style="user-select: none; -webkit-user-select: none; position:fixed; right: 30px; top: 70px; transform: rotate(-8deg)">orphy!</h2>
+            <h2 class="decor" style="user-select: none; -webkit-user-select: none; position:fixed; left: 40px; bottom: 40px; transform: rotate(8deg)">:P</h2>
+            <h2 class="decor" style="user-select: none; -webkit-user-select: none; position:fixed; left: 30px; top: 130px; transform: rotate(8deg)">hehe</h2>
+            <h2 class="decor" style="user-select: none; -webkit-user-select: none; position:fixed; right: 30px; top: 70px; transform: rotate(-8deg)">orphy!</h2>
 
-            <h2 style="user-select: none; -webkit-user-select: none; position:fixed; left: 20px; top:200px; transform: rotate(9deg);"><span class="material-symbols-outlined" style:font-size="50px;">filter_vintage</span></h2>
+            <h2 class="decor" style="user-select: none; -webkit-user-select: none; position:fixed; left: 20px; top:200px; transform: rotate(9deg);"><span class="material-symbols-outlined" style:font-size="80px;">filter_vintage</span></h2>
             <svg width="110" height="110" style="position: fixed; bottom: 40px; left: 5px;">
                 <path d="M 62.4689 11.0223 C 59.0884 11.8651 57.0936 15.2564 57.9238 18.5865 c 1.6983 6.8115 9.0005 6.8665 15.3155 12.4198 c 2.7795 -7.7671 9.378 -11.6096 7.7426 -18.1688 c -0.8303 -3.3301 -4.2342 -5.3753 -7.5642 -4.5451 c -2.4219 0.6038 -4.1887 2.5449 -4.6316 4.8526 c -1.4744 -1.8297 -3.8953 -2.7266 -6.3172 -2.1228 z" fill="black"/>
             </svg>
